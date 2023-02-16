c42491dfff3cc66d36f1ff1ceaab790ada227ad8
nErNeR NeRmOn <NERneMoNr@outlook.com>
2023-02-16 10:46:40 +0500

    граф duke барон князь

---
+++ pz_002/Graph.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_002
+{
+    internal class Graph
+    {
+        private int size; // количество вершин в графе
+        private Node[]? adjList; // ссылка на список смежности вершин графа
+        private bool[,]? adjacency; // матрица смежности графа
+        private bool[]? vector; // вектор посещенных вершин
+
+        public int Size { get; set; } // свойства
+        public Node[] AdjList { get; set; }
+        public bool[,] Adjacency { get; set; }
+        public bool[] Vector { get; set; }
+
+        public Graph(int size) // конструктор
+        { // создание и инициализация списка смежности вершин графа
+            AdjList = new Node[size];
+            for (int i = 0; i <= size; i++) AdjList[i] = null;
+        }
+        public Graph(int size, bool[,] G) // конструктор класса «Графы»
+        {
+            if (G.GetLength(0) == size && G.GetLength(1) == size)
+            {
+                adjList = new Node[size];
+                Adjacency = new bool[size, size]; // инициализация матрицы смежности
+                Adjacency = G;
+                Vector = new bool[size];
+                Size = size;
+                for (int i = 0; i < size; i++) adjList[i] = new Node(i, Program.uuu.Next(0, 10));
+                for (int i = 0; i < size; i++)
+                {
+
+                    Console.WriteLine("creating grapgh.." + new String('.', i));
+                    Vector[i] = false;                  // иниц-я вектора посещенных вершин
+                    for (int j = 0; j < size; j++)
+                    {
+                        System.Threading.Thread.Sleep(50);
+                        //(G[i, j] == true ?  : null);
+
+                        if (G[i, j] == true)
+                        {
+                            adjList[i].Link.Add(adjList[j]);
+                            Console.WriteLine($"is road(link) from \t{i + 1}\tto\t{j + 1}");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Program.Print("something wrong! LEGO(конструктор) isn't work!!");
+                size = -1;
+                adjList = null;
+                adjacency = null;
+                vector = null;
+            }
+
+        }
+    }
+}
---
+++ pz_002/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace pz_002
+{
+    internal class Program
+    {
+        public static Random uuu;
+
+        public static void Print(object a)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine(a.ToString());
+            Console.ResetColor();
+        }
+
+        /*
+         1 найти самый дешевый узел
+         2 обновить стоимость его соседей
+         3 повторять для всех узлов
+         4 результат
+         */
+
+        public static Node? FindLowestCostNode(Node[] n)
+        {
+            Node result = new Node(-1, int.MaxValue);
+
+            foreach (var item in n) if (item.Weight < result.Weight) result = item;
+
+            return result.Weight == int.MaxValue ? null : result;
+        }
+
+        public static void Dijkstra(Graph g)        //  еще что то про родителей
+        {
+            int currentWeight = 0;
+            int newWeight = 0;
+            List<Node> neighbors = new();
+
+            List<int> weights = new();
+            for (int i = 0; i < g.Size; i++) weights.Add(g.AdjList[i].Weight);
+
+            Node a = FindLowestCostNode(g.AdjList);
+
+            while (a != null)
+            {
+                currentWeight = weights[a.Id];
+                neighbors = a.Link;
+
+                foreach (var item in neighbors)
+                {
+                    newWeight = currentWeight + item.Weight;
+                    if (weights[a.Id] > newWeight)
+                    {
+                        weights[a.Id] = newWeight;
+                    }
+                }
+                g.Vector[a.Id] = true;
+
+                a = FindLowestCostNode(g.AdjList);
+            }
+
+            Console.WriteLine(currentWeight + " " + newWeight + "\n" + String.Join(';', neighbors.Select(x => x.Weight)));
+        }
+
+        public static void DFS(Graph graph, int node)
+        {
+            graph.Vector[node] = true; // отметить вершину i как обработанную
+            Console.Write("{0}" + ' ', node); // распечатать номер посещенной вершины
+            for (int k = graph.Size - 1; k >= 0; k--)
+            {
+                if (graph.Adjacency[node, k] && !(graph.Vector[k])) DFS(graph, k);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            uuu = new();
+            Console.WriteLine("\n——————————");
+
+            bool[,] O2 = new bool[5, 5]
+                            {
+                                            {false, false, false,true,false}, // матрица смежности графа G5.1  -- ориент
+                                            {true,false,false,false,true},
+                                            {false,true,false,true,false},
+                                            {true,true,true,false,true},
+                                            {false,false,false,true,false}
+                            };
+            Graph graph4 = new Graph(5, O2);
+
+            Console.Write("\ndepth: ");
+            DFS(graph4, 2);
+            Console.WriteLine("\n\n\n");
+
+            Dijkstra(graph4);
+        }
+    }
+}
