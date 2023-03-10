e2a8319447de5848704baa1c7a36955979733b5f
nErNeR NeRmOn <NERneMoNr@outlook.com>
2023-03-11 00:38:52 +0500

    пз5 , реализовано всё: глубокое клонирование и сравнение, IComparable реализован двумя решениями.

---
+++ pz_005/Country.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_005
+{
+    internal class Country : Maps
+    {
+        public double Volume { get; set; }
+
+        public Country()
+        {
+
+        }
+
+        public Country(int id, string name, double volume) : base(id, name)
+        {
+            Volume = volume;
+        }
+    }
+}
---
+++ pz_005/Maps.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_005
+{
+    internal class Maps
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Maps()
+        {
+
+        }
+
+        public Maps(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
